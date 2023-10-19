fcbb0643f3d7a4a90beb2593e47413de416d00d6
Pedro Artur <71746213+Pedro-Artur-RF@users.noreply.github.com>
2023-10-19 15:54:34 -0300

    1st commit - aula 2 JS

---
+++ recomposixaum/intro.js
@@ -0,0 +1,32 @@
+// let variavel1 = "Comi o cu de quem tá lendo"
+
+// {
+//     let variavel2 = "comi o cu de uqem tá vendo"
+//     console.log(variavel1)
+//     console.log(variavel2)
+// }
+
+// console.log(variavel1)
+// console.log(variavel2)
+
+
+//Estrutura conciciomnal
+
+// let idade = 18
+
+// if(18 === "18" || 15 == 15) {
+//     console.log("AI CALIQUINHA")
+// } else if(5 >= 5 && 6 == 6) {
+//     console.log("AI CALIQUINHA")
+// } else {
+//     console.log("AI CALIQUINHA")
+// }
+
+//REPETIÇÃ1
+
+for(let i = 0;  i <= 10; i++){
+    console.log("Te comi ", i, " vezes");
+    if (i == 5){
+        console.log("AIIII MEU CUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUU")
+    }
+}
